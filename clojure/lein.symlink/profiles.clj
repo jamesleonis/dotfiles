1cf05160323624511ccd326c791315345c41cb81
James Leonis <me@jamesleonis.com>
2015-10-02 23:26:27 -0700

    Add cljfmt to profile

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,4 +1,5 @@
-{ :user {:plugins [[cider/cider-nrepl "0.9.1"]
+{ :user {:dependencies [[cljfmt "0.3.0"]]
+         :plugins [[cider/cider-nrepl "0.9.1"]
                    [lein-exec "0.3.5"]
                    [lein-ancient "0.6.7"]
                    [lein-try "0.4.3"]]}}
