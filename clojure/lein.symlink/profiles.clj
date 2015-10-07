d2fb8b7ab46779dac9ab588c8b26a3dd855b87db
James Leonis <me@jamesleonis.com>
2015-10-06 21:29:54 -0700

    Add cljfmt to lein and vim

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,5 +1,6 @@
 { :user {:dependencies [[cljfmt "0.3.0"]]
          :plugins [[cider/cider-nrepl "0.9.1"]
                    [lein-exec "0.3.5"]
+                   [lein-cljfmt "0.3.0"]
                    [lein-ancient "0.6.7"]
                    [lein-try "0.4.3"]]}}
