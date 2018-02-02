e9026f7fd454deddb3c84d12d396db559dd03770
James Leonis <me@jamesleonis.com>
2018-02-02 14:22:26 -0800

    Update clojure profile dependencies

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,4 +1,9 @@
-{:user {:dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+{:user {:dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                        [org.clojure/tools.namespace "0.3.0-alpha4"]]
-        :plugins [[cider/cider-nrepl "0.15.0"]
-                  [lein-ancient "0.6.7"]]}}
+        :plugins [[cider/cider-nrepl "0.15.1"]]
+        :signing {:gpg-key "420E623E"}}
+ :auth {:repository-auth
+        {#"clojars"
+         {:username "jamesleonis"
+          :password "g9J5tgTQnqVq9srR2mLR"}}}
+ }
