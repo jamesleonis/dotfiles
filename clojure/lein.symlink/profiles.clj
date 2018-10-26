a48138c4237d21c1217327d2b9fc21444a2c83a0
James Leonis <me@jamesleonis.com>
2018-10-26 10:46:59 -0700

    Clean up lein config

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,6 +1,5 @@
-{:user {:dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                       [org.clojure/tools.namespace "0.3.0-alpha4"]]
-        :plugins [[cider/cider-nrepl "0.15.1"]]
+{:user {:dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]]
+        :plugins [[cider/cider-nrepl "0.18.0"]]
         :signing {:gpg-key "420E623E"}}
  :auth {:repository-auth
         {#"clojars"
