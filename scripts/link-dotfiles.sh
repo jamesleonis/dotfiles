d0ada491c385dfc8ec2e18d97c0033e2444ff2a6
James Leonis <me@jamesleonis.com>
2018-12-13 10:41:23 -0800

    Make sed commands cross-compatable

--- scripts/link-dotfiles.sh
+++ scripts/link-dotfiles.sh
@@ -2,8 +2,17 @@
 
 DOTFILES=$(find $(cd `dirname $0`/..; pwd) ! -path "*.git*" -iname "*symlink")
 
+# I hate that this is a thing.
+if [ "$(uname)" == "Darwin" ]; then
+  SED='sed -E'
+else
+  SED='sed -r'
+fi
+
 for df in $DOTFILES; do
-  target=$(echo $df | sed -r "s:.*/(.*).symlink:$HOME/.\1:")
+  target=$(echo $df | $SED "s:.*/(.*).symlink:$HOME/.\1:")
   rm -rf $target
   ln -s $df $target
 done
+
+unset SED
