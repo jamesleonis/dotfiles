e5e98f7b363732ab2139b00322fb391947444db6
James Leonis <me@jamesleonis.com>
2018-12-12 18:39:18 -0800

    Yeah...

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,8 +1,4 @@
 {:user {:dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]]
         :plugins [[cider/cider-nrepl "0.18.0"]]
         :signing {:gpg-key "420E623E"}}
- :auth {:repository-auth
-        {#"clojars"
-         {:username "jamesleonis"
-          :password "g9J5tgTQnqVq9srR2mLR"}}}
  }
