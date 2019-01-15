8aec8afa133ee390adfe5333efebdbfa527874ef
James Leonis <me@jamesleonis.com>
2019-01-14 16:22:14 -0800

    Update cider nrepl

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                        [cljfmt "0.6.3"]
                        [nrepl "0.3.1"]]
-        :plugins [[cider/cider-nrepl "0.18.0"]]
+        :plugins [[cider/cider-nrepl "0.19.0"]]
         :signing {:gpg-key "420E623E"}}
  }
