e01a57b91d63265874f7ef5d1c8d260fdc872b17
James Leonis <me@jamesleonis.com>
2019-01-26 02:03:43 +0000

    Add cljfmt for clojure and vim

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,6 +1,5 @@
 {:user {:dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                       [cljfmt "0.6.3"]
-                       [nrepl "0.3.1"]]
+                       [cljfmt "0.6.3"]]
         :plugins [[cider/cider-nrepl "0.19.0"]]
         :signing {:gpg-key "420E623E"}}
  }
