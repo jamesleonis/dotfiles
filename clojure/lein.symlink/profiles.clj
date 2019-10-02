b8ca7200ab47f250fb2cb909094d3a631b0b54fc
James Leonis <me@jamesleonis.com>
2019-10-02 12:03:26 -0700

    Update Leiningen profiles to 1.10

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,6 +1,5 @@
-{:user {:dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+{:user {:dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
                        [cljfmt "0.6.4"]]
-        :plugins [[cider/cider-nrepl "0.19.0"]
-                  [jonase/eastwood "0.3.5"]]
+        :plugins [[cider/cider-nrepl "0.22.3"]]
         :signing {:gpg-key "420E623E"}}
  }
