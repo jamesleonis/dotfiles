0208f25016fa18f5f6ec51ec2042539348ee6f7f
James Leonis <me@jamesleonis.com>
2021-09-15 14:35:44 -0700

    Clojure 1.10.1 -> 1.10.3

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+{:user {:dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
                        [cljfmt "0.6.4"]]
         :plugins [[cider/cider-nrepl "0.22.3"]]
         :signing {:gpg-key "420E623E"}}
